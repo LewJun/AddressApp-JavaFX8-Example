520fe3c5d33cd1c73935dcbc661ad794803bafd5
LewJun <lewjun072@sina.com>
2020-04-29 19:52:09 +0800

    添加showAndWait和show方法

--- Part1/src/main/java/com/example/lewjun/BaseApp.java
+++ Part1/src/main/java/com/example/lewjun/BaseApp.java
@@ -4,21 +4,79 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * @author LewJun
  */
 public abstract class BaseApp extends Application {
+
+    /**
+     * 显示并等待
+     *
+     * @param stage Stage
+     * @throws Exception
+     */
+    public void showAndWait(final Stage stage) throws Exception {
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setResizable(false);
+        initScene(stage);
+        stage.showAndWait();
+    }
+
+    /**
+     * 显示并等待
+     *
+     * @throws Exception
+     */
+    public void showAndWait() throws Exception {
+        showAndWait(newStage());
+    }
+
+    /**
+     * 显示
+     *
+     * @throws Exception
+     */
+    public void show() throws Exception {
+        show(newStage());
+    }
+
+    /**
+     * 显示
+     *
+     * @param stage Stage
+     * @throws Exception
+     */
+    public void show(final Stage stage) throws Exception {
+        initScene(stage);
+        stage.show();
+    }
+
+    private Stage newStage() {
+        return new Stage();
+    }
+
     @Override
     public void start(final Stage stage) throws Exception {
+        show(stage);
+    }
+
+    /**
+     * 初始化Scene
+     *
+     * @param stage Stage
+     * @throws IOException
+     */
+    private void initScene(final Stage stage) throws IOException {
         final Parent root = FXMLLoader.load(getClass().getResource(getResName()));
 
         initStage(stage);
 
         stage.setScene(new Scene(root));
-
-        stage.show();
     }
 
     /**
