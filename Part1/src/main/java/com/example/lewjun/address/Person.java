eb9fc1b643ab840abb752eae306800ee2c87e30a
LewJun <lewjun072@sina.com>
2020-04-29 10:50:25 +0800

    给属性添加set和get方法

--- Part1/src/main/java/com/example/lewjun/address/Person.java
+++ Part1/src/main/java/com/example/lewjun/address/Person.java
@@ -11,12 +11,20 @@ import java.time.LocalDate;
  */
 public class Person {
 
-    public StringProperty firstName;
-    public StringProperty lastName;
-    public StringProperty street;
-    public IntegerProperty postalCode;
-    public StringProperty city;
-    public ObjectProperty<LocalDate> birthday;
+    private final StringProperty firstName;
+    private final StringProperty lastName;
+    private final StringProperty street;
+    private final IntegerProperty postalCode;
+    private final StringProperty city;
+    private final ObjectProperty<LocalDate> birthday;
+
+    /**
+     * Default constructor.
+     */
+    public Person() {
+        this(null, null);
+    }
+
 
     public Person(final String firstName, final String lastName) {
         this.firstName = new SimpleStringProperty(firstName);
@@ -29,4 +37,75 @@ public class Person {
         this.birthday = new SimpleObjectProperty<>(LocalDate.of(1999, 2, 21));
     }
 
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public StringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public StringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public String getStreet() {
+        return street.get();
+    }
+
+    public void setStreet(final String street) {
+        this.street.set(street);
+    }
+
+    public StringProperty streetProperty() {
+        return street;
+    }
+
+    public int getPostalCode() {
+        return postalCode.get();
+    }
+
+    public void setPostalCode(final int postalCode) {
+        this.postalCode.set(postalCode);
+    }
+
+    public IntegerProperty postalCodeProperty() {
+        return postalCode;
+    }
+
+    public String getCity() {
+        return city.get();
+    }
+
+    public void setCity(final String city) {
+        this.city.set(city);
+    }
+
+    public StringProperty cityProperty() {
+        return city;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday.get();
+    }
+
+    public void setBirthday(final LocalDate birthday) {
+        this.birthday.set(birthday);
+    }
+
+    public ObjectProperty<LocalDate> birthdayProperty() {
+        return birthday;
+    }
 }
