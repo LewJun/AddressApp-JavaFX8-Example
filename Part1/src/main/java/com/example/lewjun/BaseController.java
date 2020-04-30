b922e968de67550ad6681a643f1797ae2ec19d1d
LewJun <lewjun072@sina.com>
2020-04-30 15:41:10 +0800

    将Controller注册到EventBus中

--- Part1/src/main/java/com/example/lewjun/BaseController.java
+++ Part1/src/main/java/com/example/lewjun/BaseController.java
@@ -1,6 +1,9 @@
 package com.example.lewjun;
 
+import com.example.lewjun.util.EventBusUtil;
 import javafx.fxml.Initializable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,8 +12,11 @@ import java.util.ResourceBundle;
  * @author LewJun
  */
 public abstract class BaseController implements Initializable {
+    protected static final Logger logger = LoggerFactory.getLogger(BaseController.class);
+
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
-
+        // 将Controller注册到EventBus中
+        EventBusUtil.register(this);
     }
 }
